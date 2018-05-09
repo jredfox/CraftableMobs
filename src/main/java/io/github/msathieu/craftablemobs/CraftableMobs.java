881c73f9ae031542345b8428f7f6000fa80abf30
MSathieu <18145111+MSathieu@users.noreply.github.com>
2018-05-09 06:55:15 +0200

    Use camelCase and change spawn eggs to own tab

--- src/main/java/io/github/msathieu/craftablemobs/CraftableMobs.java
+++ src/main/java/io/github/msathieu/craftablemobs/CraftableMobs.java
@@ -17,9 +17,16 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class CraftableMobs {
   @EventHandler
   public void init(FMLInitializationEvent event) {
+    CreativeTabs spawnEggs = new CreativeTabs("spawnEggs") {
+      @Override
+      public ItemStack getTabIconItem() {
+        return new ItemStack(Items.SPAWN_EGG);
+      }
+    };
     Blocks.MOB_SPAWNER.setCreativeTab(CreativeTabs.DECORATIONS);
-    ResourceLocation eggs_group = new ResourceLocation("craftablemobs:eggs");
-    GameRegistry.addShapedRecipe(new ResourceLocation("craftablemobs:egg"), eggs_group, new ItemStack(Items.SPAWN_EGG, 9), new Object[] {
+    Items.SPAWN_EGG.setCreativeTab(spawnEggs);
+    ResourceLocation eggsGroup = new ResourceLocation("craftablemobs:eggs");
+    GameRegistry.addShapedRecipe(new ResourceLocation("craftablemobs:egg"), eggsGroup, new ItemStack(Items.SPAWN_EGG, 9), new Object[] {
       "III",
       "IGI",
       "III",
@@ -33,53 +40,53 @@ public class CraftableMobs {
       'B', Blocks.IRON_BARS,
       'D', Blocks.DIAMOND_BLOCK
     });
-    register_egg("zombie", Items.ROTTEN_FLESH);
-    register_egg("spider", Items.STRING);
-    register_egg("enderman", Items.ENDER_PEARL);
-    register_egg("blaze", Items.BLAZE_POWDER);
-    register_egg("cave_spider", Items.SPIDER_EYE);
-    register_egg("wither_skeleton", Items.STONE_SWORD);
-    register_egg("creeper", Items.GUNPOWDER);
-    register_egg("skeleton", Items.BONE);
-    register_egg("zombie_pigman", Items.GOLD_NUGGET);
-    register_egg("magma_cube", Items.MAGMA_CREAM);
-    register_egg("guardian", Items.PRISMARINE_SHARD);
-    register_egg("pig", Items.PORKCHOP);
-    register_egg("cow", Items.BEEF);
-    register_egg("chicked", Items.FEATHER);
-    register_egg("villager", Items.EMERALD);
-    register_egg("sheep", Blocks.WOOL);
-    register_egg("silverfish", Blocks.STONE);
-    register_egg("ghast", Items.GHAST_TEAR);
-    register_egg("ocelot", Items.FISH);
-    register_egg("horse", Items.LEATHER);
-    register_egg("rabbit", Items.RABBIT);
-    register_egg("llama", Blocks.CARPET);
-    register_egg("parrot", Items.COOKIE);
-    register_egg("mooshroom", Blocks.RED_MUSHROOM);
-    register_egg("witch", Items.SPLASH_POTION);
-    register_egg("slime", Items.SLIME_BALL);
-    register_egg("wolf", Items.COOKED_BEEF);
-    register_egg("elder_guardian", Blocks.SEA_LANTERN);
-    register_egg("bat", Items.FLINT);
-    register_egg("endermite", Items.ENDER_EYE);
-    register_egg("polar_bear", Blocks.SNOW);
-    register_egg("shulker", Items.SHULKER_SHELL);
-    register_egg("vindication_illager", Items.IRON_AXE);
-    register_egg("evocation_illager", Items.TOTEM_OF_UNDYING);
-    register_egg("zombie_villager", Items.GOLDEN_APPLE);
-    register_egg("donkey", Blocks.CHEST);
-    register_egg("vex", Items.IRON_SWORD);
-    register_egg("skeleton_horse", Blocks.BONE_BLOCK);
+    registerEgg("zombie", Items.ROTTEN_FLESH);
+    registerEgg("spider", Items.STRING);
+    registerEgg("enderman", Items.ENDER_PEARL);
+    registerEgg("blaze", Items.BLAZE_POWDER);
+    registerEgg("cave_spider", Items.SPIDER_EYE);
+    registerEgg("wither_skeleton", Items.STONE_SWORD);
+    registerEgg("creeper", Items.GUNPOWDER);
+    registerEgg("skeleton", Items.BONE);
+    registerEgg("zombie_pigman", Items.GOLD_NUGGET);
+    registerEgg("magma_cube", Items.MAGMA_CREAM);
+    registerEgg("guardian", Items.PRISMARINE_SHARD);
+    registerEgg("pig", Items.PORKCHOP);
+    registerEgg("cow", Items.BEEF);
+    registerEgg("chicked", Items.FEATHER);
+    registerEgg("villager", Items.EMERALD);
+    registerEgg("sheep", Blocks.WOOL);
+    registerEgg("silverfish", Blocks.STONE);
+    registerEgg("ghast", Items.GHAST_TEAR);
+    registerEgg("ocelot", Items.FISH);
+    registerEgg("horse", Items.LEATHER);
+    registerEgg("rabbit", Items.RABBIT);
+    registerEgg("llama", Blocks.CARPET);
+    registerEgg("parrot", Items.COOKIE);
+    registerEgg("mooshroom", Blocks.RED_MUSHROOM);
+    registerEgg("witch", Items.SPLASH_POTION);
+    registerEgg("slime", Items.SLIME_BALL);
+    registerEgg("wolf", Items.COOKED_BEEF);
+    registerEgg("elder_guardian", Blocks.SEA_LANTERN);
+    registerEgg("bat", Items.FLINT);
+    registerEgg("endermite", Items.ENDER_EYE);
+    registerEgg("polar_bear", Blocks.SNOW);
+    registerEgg("shulker", Items.SHULKER_SHELL);
+    registerEgg("vindication_illager", Items.IRON_AXE);
+    registerEgg("evocation_illager", Items.TOTEM_OF_UNDYING);
+    registerEgg("zombie_villager", Items.GOLDEN_APPLE);
+    registerEgg("donkey", Blocks.CHEST);
+    registerEgg("vex", Items.IRON_SWORD);
+    registerEgg("skeleton_horse", Blocks.BONE_BLOCK);
   }
-  private void register_egg(String mob, Block ingredient) {
-    register_egg(mob, Item.getItemFromBlock(ingredient));
+  private void registerEgg(String mob, Block ingredient) {
+    registerEgg(mob, Item.getItemFromBlock(ingredient));
   }
-  private void register_egg(String mob, Item ingredient) {
-    ResourceLocation eggs_group = new ResourceLocation("craftablemobs:eggs");
+  private void registerEgg(String mob, Item ingredient) {
+    ResourceLocation eggsGroup = new ResourceLocation("craftablemobs:eggs");
     ItemStack egg = new ItemStack(Items.SPAWN_EGG);
     ItemMonsterPlacer.applyEntityIdToItemStack(egg, new ResourceLocation("minecraft:" + mob));
-    GameRegistry.addShapedRecipe(new ResourceLocation("craftablemobs:" + mob + "_egg"), eggs_group, egg, new Object[] {
+    GameRegistry.addShapedRecipe(new ResourceLocation("craftablemobs:" + mob + "_egg"), eggsGroup, egg, new Object[] {
       "III",
       "IEI",
       "III",
